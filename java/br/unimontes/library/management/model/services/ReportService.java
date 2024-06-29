589437cdfe8f5281edb1ea0de6e0b84c43317e49
marcelomatheus <marcelomatheusbr@gmail.com>
2024-06-29 16:54:19 -0300

    Service report working

--- java/br/unimontes/library/management/model/services/ReportService.java
+++ java/br/unimontes/library/management/model/services/ReportService.java
@@ -6,8 +6,6 @@
 package br.unimontes.library.management.model.services;
 import java.sql.Connection;
 
-import java.sql.DriverManager;
-
 import net.sf.jasperreports.engine.JasperFillManager;
 
 import net.sf.jasperreports.engine.JasperPrint;
@@ -18,28 +16,39 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 
 import net.sf.jasperreports.view.JasperViewer;
 import br.unimontes.library.management.model.dao.DBSingleton;
-import java.sql.SQLException;
+
 
 /**
  *
  * @author marce
  */
 public class ReportService {
-    Connection conn = null;
-    private Connection RSConnect() throws SQLException{
-        return DBSingleton.getInstance().getConnection();
-    }
-    
+     
+    //Generate report book for publisher
     public void reportPublisher(){
        
-            try (conn=RSConnceet()) {
-                String reportPath = "C:\Temp\reportPublisher.jrxml";
+            try (Connection conn = DBSingleton.getInstance().getConnection()) {
+                String reportPath = "C:\\Temp\\reportPublisher.jrxml";
                 JasperReport jr = JasperCompileManager.compileReport(reportPath);
-                JasperPrint jp = JasperFillManager.fillReport(jr, null, con);
+                JasperPrint jp = JasperFillManager.fillReport(jr, null, conn);
                 JasperViewer.viewReport(jp);
             
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
     
+    //Generate report book for author
+    public void reportAuthor(){
+       
+            try (Connection conn = DBSingleton.getInstance().getConnection()) {
+                String reportPath = "C:\\Temp\\reportAuthor.jrxml";
+                JasperReport jr = JasperCompileManager.compileReport(reportPath);
+                JasperPrint jp = JasperFillManager.fillReport(jr, null, conn);
+                JasperViewer.viewReport(jp);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
